f2f46bee01c138fba018812df62ee175811adb92
savak1990 <slava.klovan@gmai.com>
2019-06-18 17:01:17 +0300

    Refactoring...

--- hls-download.py
+++ hls-download.py
@@ -35,40 +35,59 @@ def savePlaylistToFile(playlist, path):
 	path.write(playlist)
 	path.close()
 
-def downloadSegments(media_playlist_str, base_url, dir_path):
+
+def downloadSegment(url, path, index, total, playlist_index, playlist_total):
+	print('Playlist {0}/{1}: Downloading {2}/{3} segment: {4} to {5}'.format(
+			playlist_index, playlist_total, index, total, url, path));
+	urllib.request.urlretrieve(url, path)
+
+
+def downloadSegments(media_playlist_str, base_url, dir_path, playlist_index, playlist_total):
 	media_m3u8_obj = m3u8.loads(media_playlist_str)
 	segments = media_m3u8_obj.segments.by_key(media_m3u8_obj.keys[-1])
 	for segment_index, segment in enumerate(segments):
 		segment_url = urljoin(base_url, segment.uri)
 		segment_path = dir_path.joinpath(segment.uri)
 		segment_path.parent.mkdir(parents=True, exist_ok=True)
-		print('Downloading {0}/{1} segment: {2} to {3}'.format(
-			segment_index + 1,
-			len(segments),
-			segment_url,
-			segment_path));
-		urllib.request.urlretrieve(segment_url, segment_path)
+		downloadSegment(segment_url, 
+						segment_path, 
+						segment_index + 1, 
+						len(segments), 
+						playlist_index, 
+						playlist_total)
+		
 
+def downloadMediaPlaylist(url, path, index, total):
+	print('Downloading {0}/{1} media playlist: {2} to {3}'.format(
+			index, total, url, path))
+	return urllib.request.urlopen(url).read().decode('utf-8')
+
+def downloadMediaPlaylists(master_playlist_str, base_url, dir_path):
+	master_m3u8_obj = m3u8.loads(master_playlist_str)
+	for playlist_index, playlist in enumerate(master_m3u8_obj.playlists):
+		media_playlist_url = urljoin(base_url, playlist.uri)
+		media_playlist_path = dir_path.joinpath(playlist.uri);
+
+		media_playlist = downloadMediaPlaylist(media_playlist_url, 
+				media_playlist_path, playlist_index + 1, len(master_m3u8_obj.playlists))
+		savePlaylistToFile(media_playlist, media_playlist_path)
+
+		downloadSegments(media_playlist, 
+						 media_playlist_url, 
+						 media_playlist_path.parent,
+						 playlist_index + 1,
+						 len(master_m3u8_obj.playlists))
+
+def downloadMasterPlaylist(url, path):
+	print('Downloading master playlist: {0} to {1}'.format(url, path))
+	return urllib.request.urlopen(url).read().decode('utf-8')
+
+#----------------------------------------------------------------
+#----------------------------------------------------------------
+#----------------------------------------------------------------
 
 master_playlist_path = Path(sys.argv[2]).joinpath(os.path.basename(urlparse(master_playlist_url).path))
-print('Downloading master playlist: {0} to {1}'.format(master_playlist_url, master_playlist_path))
-
-master_playlist = urllib.request.urlopen(master_playlist_url).read().decode('utf-8')
+master_playlist = downloadMasterPlaylist(master_playlist_url, master_playlist_path)
 savePlaylistToFile(master_playlist, master_playlist_path)
 
-master_m3u8_obj = m3u8.loads(master_playlist)
-
-for playlist_index, playlist in enumerate(master_m3u8_obj.playlists):	
-	media_playlist_url = urljoin(master_playlist_url, playlist.uri)
-	media_playlist_path = master_playlist_path.parent.joinpath(playlist.uri);
-	
-	print('Downloading {0}/{1} media playlist: {2} to {3}'.format(
-			playlist_index + 1, 
-			len(master_m3u8_obj.playlists), 
-			media_playlist_url, 
-			media_playlist_path))
-
-	media_playlist = urllib.request.urlopen(media_playlist_url).read().decode('utf-8')
-	savePlaylistToFile(media_playlist, media_playlist_path)
-
-	downloadSegments(media_playlist, media_playlist_url, media_playlist_path.parent)
+downloadMediaPlaylists(master_playlist, master_playlist_url, master_playlist_path.parent)
